d7d815f28415688e6a5df0e8cbdd18d5954695b6
Damian Roiz <damianx989@gmail.com>
2023-04-22 10:22:54 -0400

    links to directory and bg image set to lower res

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarItems = document.getElementsByClassName("navbar-items")[0];
 
-const navbarItems = document.getElementsByClassName("navbar-items")[0]
+toggleButton.addEventListener("click", () => {
+  navbarItems.classList.toggle("active");
+});
 
-toggleButton.addEventListener('click', ()=> {
-    navbarItems.classList.toggle('active')
-})
+$(document).ready(function () {
+  $(".btn").click(function () {
+    $("nav").toggleClass("show");
+    $(".items").toggleClass("show");
+    $("ul li").toggleClass("hide");
+  });
+});
